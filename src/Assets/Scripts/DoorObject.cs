89ed1ae23ff275b3619d4993150632858139cb3c
dapken <d.apken@neusta.de>
2017-10-22 14:58:41 +0200

    Add generic Key / Door Objects

--- src/Assets/Scripts/DoorObject.cs
+++ src/Assets/Scripts/DoorObject.cs
@@ -2,18 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractiveObject : MonoBehaviour {
+public class DoorObject : MonoBehaviour {
 
 	private bool isDone;
 	public GameObject key;
 
-	public GameObject doneEvent;
-
-	public GameObject gameOverText;
+	private AudioSource idleAudio, touchAudio, doneAudio;
 
 	// Use this for initialization
 	void Start () {
-		
+		AudioSource[] sources = GetComponents<AudioSource> ();
+		idleAudio = sources [0];
+		touchAudio = sources [1];
+		doneAudio = sources [2];
 	}
 	
 	// Update is called once per frame
@@ -29,10 +30,12 @@ public class InteractiveObject : MonoBehaviour {
 		if (other.GetComponent<Inventory> ()) {
 			Inventory inventory = other.GetComponent<Inventory> ();
 			if (inventory.ContainsItem (key.name)) {
-				gameOverText.SetActive (true);
+				gameObject.GetComponent<SpriteRenderer> ().enabled = false;
+				gameObject.GetComponent<Collider2D> ().enabled = false;
+				isDone = true;
+				idleAudio.Stop ();
+				doneAudio.Play ();
 			}
-			
-			isDone = true;
 		}
 	}
 }
